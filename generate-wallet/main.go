ca12af90cab0fdd78b0e2baf88f9d3251470ca6d
Fuga14 <vladyslavpereder@gmail.com>
2025-01-29 00:17:06 +0200

    fix: small fix

--- generate-wallet/main.go
+++ generate-wallet/main.go
@@ -10,28 +10,28 @@ import (
 )
 
 func main() {
-	// 1. Generate private key
-	privateKey, err := crypto.GenerateKey()
+	// Generate private key
+	privateKey, err := crypto.GenerateKey() // 64 hex characters
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Make the private key in readable format
 	privateKeyBytes := crypto.FromECDSA(privateKey)
-	fmt.Println("private key: ", hexutil.Encode(privateKeyBytes)[2:])
+	fmt.Println("Private key: ", hexutil.Encode(privateKeyBytes)[2:])
 
-	// 2. Generate public key
+	// Generate public key
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
-		log.Fatal("error casting public key to ECDSA")
+		log.Fatal("Error casting public key to ECDSA")
 	}
 
 	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
-	fmt.Println("public key: ", hexutil.Encode(publicKeyBytes)[4:])
+	fmt.Println("Public key: ", hexutil.Encode(publicKeyBytes)[4:])
 
-	// 3. Address
+	// Address
 	// Keccak-256 hash of the public key, and then we take the last 40 characters (20 bytes) and prefix it with 0x
 	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
-	fmt.Println("address: ", address)
+	fmt.Println("Address: ", address)
 }
